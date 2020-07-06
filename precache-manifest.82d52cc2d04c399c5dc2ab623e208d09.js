47a7cbd72703b800cbb4e29102b3839754158d16
Simona <see.moona@yahoo.com>
2020-07-06 12:47:48 +0200

    Updates

---
+++ precache-manifest.82d52cc2d04c399c5dc2ab623e208d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229de847e3194793a8b78307adb66e6b",
+    "url": "/to-do-list-in-React/index.html"
+  },
+  {
+    "revision": "e8a603520e1443f61483",
+    "url": "/to-do-list-in-React/static/css/main.ba66d5d7.chunk.css"
+  },
+  {
+    "revision": "192730f792415c944966",
+    "url": "/to-do-list-in-React/static/js/2.fdd304ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-in-React/static/js/2.fdd304ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a603520e1443f61483",
+    "url": "/to-do-list-in-React/static/js/main.453a66b8.chunk.js"
+  },
+  {
+    "revision": "94826f5b3651d0fd6460",
+    "url": "/to-do-list-in-React/static/js/runtime-main.dfe9fad5.js"
+  }
+]);
