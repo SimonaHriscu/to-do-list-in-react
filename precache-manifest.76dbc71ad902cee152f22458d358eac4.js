fe661b1b293b7f349042e467a08a37fbb42256c8
Simona <see.moona@yahoo.com>
2020-07-06 17:19:19 +0200

    Updates

---
+++ precache-manifest.76dbc71ad902cee152f22458d358eac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6e07ec746de314abdfd2cadbc64736",
+    "url": "/to-do-list-in-react/index.html"
+  },
+  {
+    "revision": "668017608a019b7e394d",
+    "url": "/to-do-list-in-react/static/css/main.c865efaa.chunk.css"
+  },
+  {
+    "revision": "192730f792415c944966",
+    "url": "/to-do-list-in-react/static/js/2.fdd304ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-in-react/static/js/2.fdd304ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668017608a019b7e394d",
+    "url": "/to-do-list-in-react/static/js/main.412f1cf9.chunk.js"
+  },
+  {
+    "revision": "7842f735d439fe910f8e",
+    "url": "/to-do-list-in-react/static/js/runtime-main.cd9f6680.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to-do-list-in-React/precache-manifest.9b7d13185817e6189df43b2bd3d0a641.js"
+  "/to-do-list-in-react/precache-manifest.76dbc71ad902cee152f22458d358eac4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to-do-list-in-React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to-do-list-in-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
