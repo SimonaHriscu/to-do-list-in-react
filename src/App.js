13785bb7f2d6515af4caee06832944ba94f34924
Simona <see.moona@yahoo.com>
2020-07-06 12:41:04 +0200

    delete button added

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component, Fragment } from "react";
+import ListOfItems from "./components/ListOfItems";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInput: "",
+      result: [],
+    };
+  }
+
+  render() {
+    this.changeHandle = (e) => {
+      e.preventDefault();
+      const userText = e.target.value.trim();
+      // console.log(userText);
+      this.setState({
+        userInput: userText,
+      });
+      // console.log(this.state.userInput)
+    };
+
+    this.submitHandle = (e) => {
+      e.preventDefault();
+      const newItem = this.state.userInput;
+      this.setState({
+        userInput: "",
+        result: [...this.state.result, newItem],
+      });
+      // console.log(this.state.result)
+    };
+
+    this.remove = (itemToRemove) => {
+      const newArray = this.state.result.filter(
+        (item) => item !== itemToRemove
+      );
+      console.log(newArray);
+      this.setState({
+        result: newArray,
+      });
+    };
+
+
+    return (
+      <Fragment>
+        <h1>Hey, stranger, this is your list for today:</h1>
+        <form onSubmit={this.submitHandle}>
+          <input
+            type="text"
+            value={this.state.userInput}
+            onChange={this.changeHandle}
+            placeholder="your new item"
+          ></input>
+          <input type="submit" value="Add" />
+        </form>
+        <div className="items-box">
+        <ListOfItems list={this.state.result} del={this.remove} />
+        </div>
+      </Fragment>
+    );
+  }
+}
+export default App;
---
+++ src/components/ListOfItems.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ListOfItems = (props) => {
+  // console.log(props.list);
+
+  const toDoList = props.list.map((item, i) => {
+    return (
+      <li key={i}>
+        {item}
+        <div className="actions">
+        <button className="delete-btn" onClick={() => props.del(item)}>
+          ✗
+        </button>
+        <button className="delete-btn" onClick={() => props.del(item)}>
+          ✔
+        </button>
+        </div>
+      </li>
+    );
+  });
+
+  return <ul>{toDoList}</ul>;
+};
+
+export default ListOfItems;
